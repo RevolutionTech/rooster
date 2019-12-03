74708eec2b65a6d7d388fb33f3121bb43137d52a
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-12-02 21:14:42 -0800

    Use django-configurations

--- manage.py
+++ manage.py
@@ -6,11 +6,12 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rooster.settings')
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'BaseConfig')
     try:
-        from django.core.management import execute_from_command_line
+        from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+            "Couldn't import django-configurations. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
