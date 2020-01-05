08fc3c65e6a0117ad7b5909d952d13da6fbab7d1
Lucas Connors <lucas.revolutiontech@gmail.com>
2020-01-04 16:28:08 -0800

    Produce context data from BaseEvent class

--- githubapi/api.py
+++ githubapi/api.py
@@ -1,7 +1,7 @@
 from github import Github
 from social_django.utils import load_strategy
 
-from githubapi.events import event_type_from_github_event
+from githubapi.events import event_from_github_event
 
 
 class GithubAPI:
@@ -17,25 +17,13 @@ class GithubAPI:
         all_events = []
 
         for github_event in self.github_named_user.get_events():
-            event_type = event_type_from_github_event(github_event)
-            if event_type:
-                repo = github_event.repo
-                payload = github_event.payload
-                pull_request = payload["pull_request"]
-                pr_title = pull_request["title"]
-
-                all_events.append(
-                    {
-                        "created_at": github_event.created_at,
-                        "subheader": event_type.subheader,
-                        "repo": {"name": repo.name, "url": repo.html_url},
-                        "pull_request": {"title": pr_title},
-                    }
-                )
+            event = event_from_github_event(github_event)
+            if event:
+                all_events.append(event.get_context_data())
 
         sorted_events = sorted(
             all_events,
-            key=lambda event: (event["created_at"], event["subheader"].lower()),
+            key=lambda e: (e["created_at"], e["subheader"].lower()),
             reverse=True,
         )
         return sorted_events
--- githubapi/events.py
+++ githubapi/events.py
@@ -1,26 +1,40 @@
-class BaseEventType:
-    pass
+class BaseEvent:
+    def __init__(self, github_event):
+        self.github_event = github_event
+
+    def get_context_data(self):
+        repo = self.github_event.repo
+        payload = self.github_event.payload
+        pull_request = payload["pull_request"]
+        pr_title = pull_request["title"]
+
+        return {
+            "created_at": self.github_event.created_at,
+            "subheader": self.subheader,
+            "repo": {"name": repo.name, "url": repo.html_url},
+            "pull_request": {"title": pr_title},
+        }
 
 
-class PullRequestEventType(BaseEventType):
+class PullRequestEvent(BaseEvent):
     api_type = "PullRequestEvent"
     action = "opened"
     subheader = "Pull Requests"
 
 
-class PullRequestCommentEventType(BaseEventType):
+class PullRequestCommentEvent(BaseEvent):
     api_type = "PullRequestReviewCommentEvent"
     action = "created"
     subheader = "PR Reviews"
 
 
-EVENT_TYPES = [PullRequestEventType, PullRequestCommentEventType]
+EVENT_CLASSES = [PullRequestEvent, PullRequestCommentEvent]
 
 
-def event_type_from_github_event(github_event):
-    for event_type in EVENT_TYPES:
+def event_from_github_event(github_event):
+    for event_class in EVENT_CLASSES:
         if (
-            event_type.api_type == github_event.type
-            and event_type.action == github_event.payload["action"]
+            event_class.api_type == github_event.type
+            and event_class.action == github_event.payload["action"]
         ):
-            return event_type
+            return event_class(github_event)
